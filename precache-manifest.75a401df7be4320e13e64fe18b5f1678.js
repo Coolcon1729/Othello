cfd499f102e57c1f8890c31117f04d161ff2390a
Coolcon1729 <crawling@uwaterloo.ca>
2020-09-04 15:41:49 -0400

    Updates

---
+++ precache-manifest.75a401df7be4320e13e64fe18b5f1678.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ccecdee93b08bbf99d6fb2689677195",
+    "url": "/Othello/index.html"
+  },
+  {
+    "revision": "903eaa34f51f27970bc4",
+    "url": "/Othello/static/css/main.f9618bd7.chunk.css"
+  },
+  {
+    "revision": "e45d52546ee902b39c63",
+    "url": "/Othello/static/js/2.ad80ea67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Othello/static/js/2.ad80ea67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "903eaa34f51f27970bc4",
+    "url": "/Othello/static/js/main.eb09b931.chunk.js"
+  },
+  {
+    "revision": "e2a97f1c62de11aef9ec",
+    "url": "/Othello/static/js/runtime-main.0d1b787b.js"
+  }
+]);
